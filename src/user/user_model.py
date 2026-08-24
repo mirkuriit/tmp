7f5eeb9cec3c1b41b4925cf654ac7b68208a3a33
Egor Daurtsev <tot-kto-idet@yandex.ru>
2026-08-25 03:32:35 +0800

    refactor(src/user/user_model.py): fix name

--- src/user/user_model.py
+++ src/user/user_model.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.models.base_models import AuditMixin, Base
 
 
-class UserModel(AuditMixin, Base):
+class User(AuditMixin, Base):
     __tablename__ = 'users'
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     username: Mapped[str]
