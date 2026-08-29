e011fee5efffbee253fc4e34195d943afac811f9
Egor Daurtsev <tot-kto-idet@yandex.ru>
2026-08-30 03:35:13 +0800

    init(src/settings.py): initial logging level env

--- src/config.py
+++ src/config.py
@@ -1,14 +1,24 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator
 
 
 class Settings(BaseSettings):
     postgres_url: str
+    log_level: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
+
+    @field_validator("log_level")
+    @classmethod
+    def is_log_level_valid(cls, value):
+        if value in ["TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
+            return value
+        raise ValueError(f"Invalid log level: {value}")
+
 
 
 settings = Settings()
